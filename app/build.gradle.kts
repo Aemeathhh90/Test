2c8a19b889afe14ce4bcb8afc86dfa572fef3f43
Aemeathhh90 <kakavasha1210@gmail.com>
2026-09-13 19:51:48 +0700

    profile: add animated image support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("io.coil-kt:coil-compose:2.7.0")
+    implementation("io.coil-kt:coil-gif:2.7.0")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
 
