8d06551c7f4c9193b152fef8c69ddac1309078e7
Aemeathhh90 <kakavasha1210@gmail.com>
2026-09-14 23:31:28 +0700

    chore: remove unused Media3 import

--- app/src/main/java/com/kakaanime/app/player/core/PlayerCore.kt
+++ app/src/main/java/com/kakaanime/app/player/core/PlayerCore.kt
@@ -7,7 +7,6 @@ import androidx.media3.common.PlaybackParameters
 import androidx.media3.common.PlaybackException
 import androidx.media3.common.Player
 import androidx.media3.datasource.DefaultHttpDataSource
-import androidx.media3.exoplayer.DefaultLoadControl
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.exoplayer.source.DefaultMediaSourceFactory
 import com.kakaanime.app.provider.NormalizedStream
