34cb1dc3be93242c40980386db954c8271896b0b
Aemeathhh90 <kakavasha1210@gmail.com>
2026-09-15 16:56:05 +0700

    fix: use provider library factory

--- app/src/main/java/com/kakaanime/app/data/RepositoryFactory.kt
+++ app/src/main/java/com/kakaanime/app/data/RepositoryFactory.kt
@@ -1,11 +1,9 @@
 package com.kakaanime.app.data
 
-import com.kakaanime.app.provider.ProviderFactory
+import com.kakaanime.provider.ProviderFactory
 
 object RepositoryFactory {
-
     fun createAnimeRepository(): ProviderAnimeRepository {
-        val engine = ProviderFactory.createEngine()
-        return ProviderAnimeRepository(engine)
+        return ProviderAnimeRepository(ProviderFactory.createEngine())
     }
 }
