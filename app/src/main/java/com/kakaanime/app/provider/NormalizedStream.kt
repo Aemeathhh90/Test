2a364d4517f1f198eb1111b345861cc64d775750
Aemeathhh90 <kakavasha1210@gmail.com>
2026-09-15 17:01:47 +0700

    fix: expose provider stream metadata cache to app

--- app/src/main/java/com/kakaanime/app/provider/NormalizedStream.kt
+++ app/src/main/java/com/kakaanime/app/provider/NormalizedStream.kt
@@ -2,5 +2,6 @@ package com.kakaanime.app.provider
 
 /** App compatibility aliases for normalized playback models. */
 typealias NormalizedStream = com.kakaanime.provider.NormalizedStream
+typealias StreamMetadataCache = com.kakaanime.provider.StreamMetadataCache
 typealias StreamQuality = com.kakaanime.provider.StreamQuality
 typealias StreamType = com.kakaanime.provider.StreamType
