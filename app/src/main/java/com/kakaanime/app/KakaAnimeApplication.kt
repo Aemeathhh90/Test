59d354506e3184132e3ac637543430438e35bcd6
Aemeathhh90 <kakavasha1210@gmail.com>
2026-09-15 16:56:36 +0700

    fix: remove retired extractor initialization

--- app/src/main/java/com/kakaanime/app/KakaAnimeApplication.kt
+++ app/src/main/java/com/kakaanime/app/KakaAnimeApplication.kt
@@ -1,11 +1,5 @@
 package com.kakaanime.app
 
 import android.app.Application
-import com.kakaanime.app.provider.extractor.AppContextProvider
 
-class KakaAnimeApplication : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        AppContextProvider.initialize(this)
-    }
-}
+class KakaAnimeApplication : Application()
