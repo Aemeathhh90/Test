d4af7036f524615decd5867a3128c7a766f6e206
Aemeathhh90 <kakavasha1210@gmail.com>
2026-09-15 16:09:01 +0700

    build: pin provider source dependency to main branch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,7 +57,9 @@ dependencies {
     implementation("com.google.android.gms:play-services-ads:24.8.0")
     implementation("com.android.billingclient:billing:9.1.0")
 
-    implementation("com.kakaanime:provider:0.1.0")
+    implementation("com.kakaanime:provider") {
+        version { branch = "main" }
+    }
 
     androidTestImplementation("androidx.test:core:1.7.0")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
